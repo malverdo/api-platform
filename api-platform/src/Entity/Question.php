b38f631c767ea8ef92651265512295c11c01f2b1
malverdo <malverdo11@mail.ru>
2022-05-08 10:55:49 +0300

    data

--- api-platform/src/Entity/Question.php
+++ api-platform/src/Entity/Question.php
@@ -2,7 +2,39 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiSubresource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ApiResource
+ */
 class Question
 {
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
 
+    /**
+     * @ORM\Column
+     */
+    public $content;
+
+    /**
+     * @ORM\OneToOne(targetEntity="Answer", inversedBy="question")
+     * @ORM\JoinColumn(referencedColumnName="id", unique=true)
+     * @ApiSubresource
+     */
+    public $answer;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    // ...
 }
