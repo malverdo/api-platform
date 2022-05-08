7d9f95a682bbf182214189b7bebf5f688dd49c26
malverdo <malverdo11@mail.ru>
2022-05-08 09:45:30 +0300

    data

--- api-platform/src/Repository/AccauntRepository.php
+++ api-platform/src/Repository/AccauntRepository.php
@@ -4,12 +4,20 @@ namespace App\Repository;
 
 use App\Entity\Accaunt;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ObjectManager;
 
 class AccauntRepository extends ServiceEntityRepository
 {
+
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Accaunt::class);
+        $entityManager = $registry->getManager();
+
     }
+
+
 }
