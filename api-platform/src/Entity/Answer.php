3305717cc0772a5c60d037264b0fe222f341e41d
malverdo <malverdo11@mail.ru>
2022-05-08 10:55:44 +0300

    data

--- api-platform/src/Entity/Answer.php
+++ api-platform/src/Entity/Answer.php
@@ -2,7 +2,36 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ApiResource
+ */
 class Answer
 {
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
 
+    /**
+     * @ORM\Column
+     */
+    public $content;
+
+    /**
+     * @ORM\OneToOne(targetEntity="Question", mappedBy="answer")
+     */
+    public $question;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    // ...
 }
